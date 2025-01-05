65f4dfeef626f07fbe66b94f8245c68412ee3372
Sujal200507 <shujalbhai7@gmail.com>
2025-01-05 13:51:14 +0530

    Added new problem solutions

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,1 +1,27 @@
-"// Java code for reverse array problem" 
+package com.company;
+
+
+public class Main {
+    public static void reverse(int numbers[]){
+
+        int start = 0;
+        int end = numbers.length-1;
+
+        while (start<end){
+            int temp = numbers[end];
+            numbers[end] = numbers[start];
+            numbers[start] = temp;
+            start++;
+            end--;
+        }
+
+    }
+
+    public static void main(String[]args){
+        int numbers[] = {3,5,4,1,9};
+        reverse(numbers);
+        for (int i=0; i< numbers.length; i++){
+            System.out.print(numbers[i]+" ");
+        }
+    }
+}
